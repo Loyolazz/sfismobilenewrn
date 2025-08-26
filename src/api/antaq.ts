adfcc1ccd23f91798249a7c6ed99167f5a0f2120
arthur.loyola <loyolazempresaria@gmail.com>
2025-08-26 18:32:41 -0300

    Configuração de variaveis

--- src/api/antaq.ts
+++ src/api/antaq.ts
@@ -2,7 +2,18 @@ import axios from "axios";
 import { create } from "xmlbuilder2";
 import { XMLParser } from "fast-xml-parser";
 
+// DES
 const SERVICE_URL = "https://sfismobile.antaq.gov.br/AntaqService/Services.asmx";
+
+// HMG TRAINEE
+// const SERVICE_URL = "https://10.111.2.68/AntaqServiceTRN/Services.asmx";
+
+// MAQUINA CRISPIMM
+// const SERVICE_URL = 'http://10.212.134.8/AntaqService/Services.asmx/';
+
+// PRD NOVO;
+// const SERVICE_URL = "https://web3.antaq.gov.br/AntaqService/Services.asmx";
+
 const parser = new XMLParser({ ignoreAttributes: false, removeNSPrefix: true });
 
 function buildEnvelope(action: string, params?: Record<string, string>) {
--- src/lib/version.ts
+++ src/lib/version.ts
@@ -2,9 +2,25 @@ import Constants from "expo-constants";
 import * as Linking from "expo-linking";
 import { getVersion as fetchVersion } from "../api/getVersion";
 
-const APK_HOST = "http://10.61.0.40";
-const APK_PATH = "/Sistemas/SFISMobile/apk";
-const APK_NAME = "SFISMobile";
+//DES
+ const APK_HOST = "http://10.61.0.40";
+ const APK_PATH = "/Sistemas/SFISMobile/apk";
+ const APK_NAME = "SFISMobile";
+
+//MAQUINA CRISPIMM
+// const APK_HOST = "http://10.212.134.8";
+// const APK_PATH = "/Sistemas/SFISMobile/apk";
+// const APK_NAME = "SFISMobile";
+
+//HMG
+// const APK_HOST = "https://sistemasinternet3hmg.antaq.gov.br";
+// const APK_PATH = "/Sistemas/SFISMobile/apk-trainee";
+// const APK_NAME = "SFISMobile";
+
+//PRD
+// const APK_HOST = "https://web3.antaq.gov.br";
+// const APK_PATH = "/Sistemas/SFISMobile";
+// const APK_NAME = "SFISMobile";
 
 export function getAppVersion() {
   return (
